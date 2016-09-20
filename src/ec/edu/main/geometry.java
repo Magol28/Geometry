965dabd626bf3af6e57f7e48fa3758b7426df3bb
Miguel_Angel <Miguel_Angel@10.9.5.49>
2016-09-20 10:07:32 -0500

    calculo de circunferencias y triangulos

---
+++ src/ec/edu/main/geometry.java
@@ -0,0 +1,15 @@
+
+package ec.edu.main;
+
+import ec.edu.geometry.circunferences.Circle;
+import ec.edu.util.GeometriConstans;
+
+public class geometry {
+
+    public static void main(String[] args) {
+        Circle Circle1= new Circle(5f);
+        ec.edu.util.Printerutil.printshare(Circle1.getClass().getSimpleName(), GeometriConstans.Perimeter, Circle1.perimeter()+"");
+      
+    }
+    
+}
